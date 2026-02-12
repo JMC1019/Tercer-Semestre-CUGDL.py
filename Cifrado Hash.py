b3ed2ccab2759cf19dc4c33f57fa972502e128c2
Josue Martin del Campo <josuenov3@pm.me>
2026-02-12 08:26:20 -0600

    Versión final Ejercicio Diagnóstico

---
+++ Cifrado Hash.py
@@ -0,0 +1,35 @@
+import hashlib
+import tkinter as tk
+from tkinter import filedialog
+
+def sha256_de_archivo(ruta_archivo, tam_bloque=4096):
+    sha256 = hashlib.sha256()
+
+    try:
+        with open(ruta_archivo, "rb") as archivo:
+            while True:
+                datos = archivo.read(tam_bloque)
+                if not datos:
+                    break
+                sha256.update(datos)
+
+        return sha256.hexdigest()
+
+    except FileNotFoundError:
+        return "Error: El archivo no existe."
+    except Exception as e:
+        return f"Error: {e}"
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.withdraw()
+
+    print("Selecciona un archivo...")
+    ruta = filedialog.askopenfilename(title="Selecciona el archivo para generar SHA-256")
+
+    if ruta:
+        hash_resultado = sha256_de_archivo(ruta)
+        print("Archivo seleccionado:", ruta)
+        print("SHA-256:", hash_resultado)
+    else:
+        print("No se seleccionó ningún archivo.")
